dba3263eb0dbc11c16e1fb5d040b77a4a1b0030d
Peter <diosdadopeter@gmail.com>
2021-05-18 13:12:13 -0700

    removed dead code

--- server/server.js
+++ server/server.js
@@ -11,25 +11,25 @@ const app = express()
 app.use(express.json())
 app.use(bodyParser.json({ limit: '30mb', extended: true}))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true}))
-app.use(cors())
 
 //cors        
 app.use(cors())
 
 //conncecting to mongoDB
 const dbURI = process.env.ATLUS_URI
+console.log(dbURI) 
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(8000, console.log('running mongodb')))
+    .then(() => app.listen(8000, console.log('connected to mongodb')))
     .catch (err => console.log(err))
 
 //routes
 const blogs = require('./routes/api/blogs')
-app.use('/blogs', blogs)
+app.use('/blogs', blogs) 
 
 //serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'))
+//     app.get('*', (req,res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     })
+// }
